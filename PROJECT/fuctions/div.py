5c5839447739d7cb29c4daf9cc90d33a5799b36c
indhuja <indhujad04@gmail.com>
2025-03-19 18:38:10 +0530

    Removed PROJECT folder

--- PROJECT/fuctions/div.py
+++
@@ -1,5 +0,0 @@
-def divide(a: float, b: float):
-    """Return the division of two numbers (avoid divide by zero)."""
-    if b == 0:
-        return {"error": "Division by zero is not allowed"}
-    return {"result": a / b}
